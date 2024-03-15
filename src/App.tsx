412768c4f694a9088b609ad0b3a97fab93222949
enypy <g404ebus@gmail.com>
2024-03-15 17:16:12 +0100

    updt

--- src/App.tsx
+++ src/App.tsx
@@ -13,8 +13,8 @@ function App() {
       <div className="mx-[20%] my-5">
         <h1 className="croll-m-20 text-4xl font-extrabold tracking-tight lg:text-5xl uppercase">Iste ipsum corrupti laborum <br /> pariatur</h1>
         <p className="text-gray-500 mt-5">Ut deserunt vel et praesentium tenetur doloribus iure itaque tempore, cupiditate distinctio.</p>
-        <Tabs className="mt-5">
-          <TabsList defaultValue="webdev" className="bg-gray-100 rounded-full p-0 text-gray-400">
+        <Tabs defaultValue="webdev" className="mt-5">
+          <TabsList className="bg-gray-100 rounded-full p-0 text-gray-400">
             <TabsTrigger value="webdev" className="px-6 h-full rounded-full data-[state=active]:bg-slate-900 data-[state=active]:text-white">Web Developement</TabsTrigger>
             <TabsTrigger value="design" className="px-6 h-full rounded-full data-[state=active]:bg-slate-900 data-[state=active]:text-white">Design</TabsTrigger>
             <TabsTrigger value="digicom" className="px-6 h-full rounded-full data-[state=active]:bg-slate-900 data-[state=active]:text-white">Communication digitale</TabsTrigger>
